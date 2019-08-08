b31408a09bb33db832e52c17d55397548cabe80f
Wookie Park <wpark1@latlmact3deg8wp.man.co>
2019-08-08 08:26:39 -0400

    added sinatra server

---
+++ app/api.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'sinatra/base'
+require 'json'
+
+module ExpenseTracker
+  # ExpenseTracker API
+  class API < Sinatra::Base
+    post '/expenses' do
+      JSON.generate('expense_id' => 42)
+    end
+
+    get '/expenses/:date' do
+      JSON.generate([])
+    end
+  end
+end
